b123ea2d73883667ad4aa72f1d8ddcfba2dec8dd
Cezar Mocanu <cmocanu@cisco.com>
2021-05-28 16:11:24 +0300

    [master] Added react router paths

--- submision-client/src/App.js
+++ submision-client/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import {VerificationDashboard} from './components/verification-dashboard/VerificationDashboard';
 
@@ -9,7 +15,24 @@ import './App.scss';
 
 function App() {
   return (
-    <VerificationDashboard />
+    <Router>
+      <Switch>
+        <Route exact path={'/'}>
+          <div>
+            <Link to="/verification">Verify</Link>
+            <Link to="/submit">Submit</Link>
+          </div>
+        </Route>
+        <Route path={'/verification'}>
+          <VerificationDashboard />
+        </Route>
+        <Route path={'/submit'}>
+          <h3>Add a file to submit</h3>
+        </Route>
+      </Switch>
+    </Router>
+    
+    
   )
 }
 
